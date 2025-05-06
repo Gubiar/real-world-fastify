bcec32891cadb04098cb95c42a4056eff0221e08
Gubiar <gubiardev@gmail.com>
2025-05-06 11:18:15 -0300

    chore(prisma): define explicit output path for generated client

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../prisma/generated/client';
 import { FastifyInstance, FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin';
 
--- src/types/prisma.d.ts
+++ src/types/prisma.d.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../prisma/generated/client';
 
 // Re-export User type from Prisma schema
 export type User = {
